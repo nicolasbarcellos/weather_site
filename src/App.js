b5da53b599efb496d3165e7c9968c4aede556fba
Nicolas Barcellos <nicolasbarcellos95@gmail.com>
2023-06-02 16:41:03 -0300

    a

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
   const [temperature, setTemperature] = useState()
   const [imgSource, setImgSource] = useState()
 
+
+  // <div id="cont_ef79e9c6a51009a9d19ef9df9707cb11"><script type="text/javascript" async src="https://www.tempo.com/wid_loader/ef79e9c6a51009a9d19ef9df9707cb11"></script></div>
+
   async function fetchTemperature() {
     const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=-22.11&lon=-45.05&appid=f5074c7a55386895a37bbedfbb872512&units=metric`)
     // const {current} = 
@@ -22,6 +25,20 @@ function App() {
     setImgSource(`http://openweathermap.org/img/w/${icon}.png`)
 
   }
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = "https://www.tempo.com/wid_loader/ef79e9c6a51009a9d19ef9df9707cb11";
+    script.async = true;
+    script.id = "cont_ef79e9c6a51009a9d19ef9df9707cb11"
+  
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, [])
 
   useEffect(() => {
   async function fetchData() {
